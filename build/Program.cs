3761d9af7237da3b93be0e861bb3e7a069cb21d3
Martijn Laarman <Mpdreamz@gmail.com>
2024-10-03 16:16:57 +0200

    Test for licences in PR

--- build/Program.cs
+++ build/Program.cs
@@ -1,11 +1,23 @@
 using ConsoleAppFramework;
+using ProcNet;
 using Zx;
-using static Zx.Env;
+//using static Zx.Env;
 
 var app = ConsoleApp.Create();
 app.Add("", async (Cancel _) =>
 {
+	await "dotnet tool restore";
 	await "dotnet build -c Release --verbosity minimal";
+});
+app.Add("validate-licenses", (Cancel _) =>
+{
+	string[] args =
+	[
+		"-u", "-t", "-i", "docs-builder.sln", "--use-project-assets-json",
+		"--forbidden-license-types", "build/forbidden-license-types.json",
+		"--packages-filter", "#System..*#"
+	];
+	Proc.Exec("dotnet", ["dotnet-project-licenses", ..args]);
 });
 
 await app.RunAsync(args);
